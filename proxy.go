c2f93b884190997b0540085a241e0e45c671668d
Karrick S. McDermott <kmcdermott@linkedin.com>
2016-12-23 14:19:20 -0500

    Proxy responds with appropriate HTTP status codes when error * supports 'Accept-Encoding: gzip'

--- proxy.go
+++ proxy.go
@@ -1,9 +1,13 @@
 package gorange
 
 import (
+	"compress/gzip"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
@@ -17,7 +21,7 @@ type ProxyConfig struct {
 // Proxy creates a proxy http server on the port that proxies range queries to the specified range
 // servers.
 func Proxy(config ProxyConfig) error {
-	defaultQuerier, err := NewQuerier(&Configurator{
+	querier, err := NewQuerier(&Configurator{
 		CheckVersionPeriodicity: config.CheckVersionPeriodicity,
 		RetryCount:              len(config.Servers),
 		Servers:                 config.Servers,
@@ -25,20 +29,56 @@ func Proxy(config ProxyConfig) error {
 	if err != nil {
 		return err
 	}
-	http.HandleFunc("/range/list", func(w http.ResponseWriter, r *http.Request) {
-		query, err := url.QueryUnescape(r.URL.RawQuery)
-		if err != nil {
-			fmt.Fprintf(w, "Invalid query: %q\r\n", query)
+	http.HandleFunc("/range/list", makeGzipHandler(makeRangeHandler(querier)))
+	return http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
+}
+
+func makeRangeHandler(querier Querier) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			logHttpError(&w, r, errors.New(r.Method), http.StatusMethodNotAllowed)
 			return
 		}
-		response, err := defaultQuerier.Query(query)
+		query, err := url.QueryUnescape(r.URL.RawQuery)
 		if err != nil {
-			fmt.Fprintf(w, "Cannot resolve query: %q; error: %s\r\n", query, err)
+			logHttpError(&w, r, fmt.Errorf("cannot decode query: %s", err), http.StatusBadRequest)
+		}
+		response, err := querier.Query(query)
+		if err != nil {
+			logHttpError(&w, r, fmt.Errorf("cannot resolve query: %s", err), http.StatusBadGateway)
 			return
 		}
 		for _, item := range response {
 			fmt.Fprintf(w, "%s\r\n", item)
 		}
-	})
-	return http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
+	}
+}
+
+func logHttpError(w *http.ResponseWriter, r *http.Request, err error, status int) {
+	http.Error(*w, err.Error(), status)
+}
+
+type gzipResponseWriter struct {
+	io.Writer
+	http.ResponseWriter
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func makeGzipHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			fn(w, r)
+			return
+		}
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		fn(gzipResponseWriter{Writer: gz, ResponseWriter: w}, r)
+		err := gz.Close()
+		if err != nil {
+			logHttpError(&w, r, fmt.Errorf("cannot compress response: %s", err), http.StatusInternalServerError)
+		}
+	}
 }
