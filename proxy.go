4d7798fd97611fcd6db8441e5cd10ce4ac4311cc
Karrick S. McDermott <kmcdermott@linkedin.com>
2017-01-26 23:05:13 -0800

    uses gohm for HTTP handler middleware

--- proxy.go
+++ proxy.go
@@ -2,14 +2,17 @@ package gorange
 
 import (
 	"context"
+	"expvar"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
-	"strconv"
-	"strings"
 	"time"
+
+	"github.com/karrick/gohm"
 )
+
+var errorCount = expvar.NewInt("errorCount")
 
 // ProxyConfig specifies the configuration for a gorange proxy HTTP server.
 type ProxyConfig struct {
@@ -51,13 +54,15 @@ func Proxy(config ProxyConfig) error {
 	mux.Handle("/range/list", onlyGet(decodeURI(list(querier))))
 	mux.Handle("/", notFound())
 
-	h := PanicHandler(mux)
+	var h http.Handler = mux
 	if config.Timeout > 0 {
-		h = http.TimeoutHandler(h, config.Timeout, "request took too long")
+		h = http.TimeoutHandler(h, config.Timeout, "took too long to process request")
 	}
+	h = gohm.PanicHandler(mux)
+	h = gohm.ErrorCountHandler(errorCount, h)
 	if config.Log != nil {
-		h = ErrorLogHandler(h, config.Log)
-		// h = LogHandler(h, config.Log)
+		h = gohm.ErrorLogHandler(config.Log, h)
+		// h = gohm.LogHandler(config.Log, h)
 	}
 
 	server := http.Server{
@@ -69,24 +74,16 @@ func Proxy(config ProxyConfig) error {
 	return server.ListenAndServe()
 }
 
-func httpError(w http.ResponseWriter, text string, code int) {
-	fullText := strconv.Itoa(code) + " " + http.StatusText(code)
-	if text != "" {
-		fullText += ": " + text
-	}
-	http.Error(w, fullText, code)
-}
-
 func notFound() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		httpError(w, fmt.Sprintf("%v", r.URL), http.StatusNotFound)
+		gohm.Error(w, fmt.Sprintf("%v", r.URL), http.StatusNotFound)
 	})
 }
 
 func onlyGet(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
-			httpError(w, r.Method, http.StatusMethodNotAllowed)
+			gohm.Error(w, r.Method, http.StatusMethodNotAllowed)
 			return
 		}
 		next.ServeHTTP(w, r)
@@ -100,7 +97,7 @@ const requestIDKey key = 0
 func contextFromQuery(ctx context.Context, r *http.Request) (context.Context, error) {
 	query, err := url.QueryUnescape(r.URL.RawQuery)
 	if err != nil {
-		return nil, fmt.Errorf("cannot decode query: " + err.Error())
+		return nil, fmt.Errorf("cannot decode query: %s", err)
 	}
 	return context.WithValue(ctx, requestIDKey, query), nil
 }
@@ -113,7 +110,7 @@ func decodeURI(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx, err := contextFromQuery(r.Context(), r)
 		if err != nil {
-			httpError(w, err.Error(), http.StatusBadRequest)
+			gohm.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		next.ServeHTTP(w, r.WithContext(ctx))
@@ -125,11 +122,11 @@ func expand(querier Querier) http.Handler {
 		query := queryFromContext(r.Context())
 		response, err := querier.Expand(query)
 		if err != nil {
-			httpError(w, "cannot resolve query: "+err.Error(), http.StatusBadGateway)
+			gohm.Error(w, "cannot resolve query: "+err.Error(), http.StatusBadGateway)
 			return
 		}
 		if _, err = io.WriteString(w, response); err != nil {
-			httpError(w, "cannot write response: "+err.Error(), http.StatusInternalServerError)
+			gohm.Error(w, "cannot write response: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 	})
@@ -140,110 +137,14 @@ func list(querier Querier) http.Handler {
 		query := queryFromContext(r.Context())
 		responses, err := querier.List(query)
 		if err != nil {
-			httpError(w, "cannot resolve query: "+err.Error(), http.StatusBadGateway)
+			gohm.Error(w, "cannot resolve query: "+err.Error(), http.StatusBadGateway)
 			return
 		}
 		for _, response := range responses {
 			if _, err = fmt.Fprintf(w, "%s\r\n", response); err != nil {
-				httpError(w, "cannot write response: "+err.Error(), http.StatusInternalServerError)
+				gohm.Error(w, "cannot write response: "+err.Error(), http.StatusInternalServerError)
 				return
 			}
 		}
-	})
-}
-
-// ErrorLogHandler returns a new http.Handler that logs HTTP requests that result in response
-// errors. The handler will output lines in common log format to the specified io.Writer.
-func ErrorLogHandler(next http.Handler, out io.Writer) http.Handler {
-	const apacheLogFormat = "%s [%s] \"%s\" %d %d %f\n"
-	const timeFormat = "02/Jan/2006:15:04:05 MST"
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		lrw := &loggedResponseWriter{
-			ResponseWriter: w,
-			status:         http.StatusOK,
-		}
-
-		begin := time.Now()
-		next.ServeHTTP(lrw, r)
-
-		if lrw.status != http.StatusOK {
-			end := time.Now()
-			clientIP := r.RemoteAddr
-			if colon := strings.LastIndex(clientIP, ":"); colon != -1 {
-				clientIP = clientIP[:colon]
-			}
-			request := fmt.Sprintf("%s %s %s", r.Method, r.RequestURI, r.Proto)
-			duration := end.Sub(begin).Seconds()
-			formattedTime := end.UTC().Format(timeFormat)
-			fmt.Fprintf(out, apacheLogFormat, clientIP, formattedTime, request, lrw.status, lrw.responseBytes, duration)
-		}
-	})
-}
-
-// LogHandler returns a new http.Handler that logs HTTP requests and responses in common log format
-// to the specified io.Writer.
-func LogHandler(next http.Handler, out io.Writer) http.Handler {
-	const apacheLogFormat = "%s [%s] \"%s\" %d %d %f\n"
-	const timeFormat = "02/Jan/2006:15:04:05 MST"
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		lrw := &loggedResponseWriter{
-			ResponseWriter: w,
-			status:         http.StatusOK,
-		}
-
-		begin := time.Now()
-		next.ServeHTTP(lrw, r)
-		end := time.Now()
-
-		clientIP := r.RemoteAddr
-		if colon := strings.LastIndex(clientIP, ":"); colon != -1 {
-			clientIP = clientIP[:colon]
-		}
-		request := fmt.Sprintf("%s %s %s", r.Method, r.RequestURI, r.Proto)
-
-		duration := end.Sub(begin).Seconds()
-		formattedTime := end.UTC().Format(timeFormat)
-		fmt.Fprintf(out, apacheLogFormat, clientIP, formattedTime, request, lrw.status, lrw.responseBytes, duration)
-	})
-}
-
-type loggedResponseWriter struct {
-	http.ResponseWriter
-	responseBytes int64
-	status        int
-}
-
-func (r *loggedResponseWriter) Write(p []byte) (int, error) {
-	written, err := r.ResponseWriter.Write(p)
-	r.responseBytes += int64(written)
-	return written, err
-}
-
-func (r *loggedResponseWriter) WriteHeader(status int) {
-	r.status = status
-	r.ResponseWriter.WriteHeader(status)
-}
-
-// PanicHandler returns a new http.Handler that catches a panic caused by the specified
-// http.Handler, and responds with an appropriate http status code and message.
-func PanicHandler(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			if r := recover(); r != nil {
-				var text string
-				switch t := r.(type) {
-				case error:
-					text = t.Error()
-				case string:
-
-				default:
-					text = fmt.Sprintf("%v", r)
-				}
-				httpError(w, text, http.StatusInternalServerError)
-			}
-		}()
-		next.ServeHTTP(w, r)
 	})
 }
