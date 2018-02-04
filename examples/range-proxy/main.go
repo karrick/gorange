ce618e4a632dd7cd116317506811bb5c99339c52
Karrick S. McDermott <kmcdermo@kmcdermo-mn1.linkedin.biz>
2018-02-04 02:58:28 -0500

    added -h, --help flag

--- examples/range-proxy/main.go
+++ examples/range-proxy/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -15,18 +16,32 @@ import (
 
 var (
 	optCheckVersion = golf.DurationP('c', "check-version", 15*time.Second, "periodicity to check %version for updates")
+	optHelp         = golf.BoolP('h', "help", false, "display program help then exit")
 	optPort         = golf.UintP('p', "port", 8081, "port to bind to")
 	optPprof        = golf.Uint("pprof", 0, "pprof port to bind to")
-	optServers      = golf.StringP('s', "servers", "", "specify comma delimited list of range servers")
+	optServers      = golf.StringP('s', "servers", "range", "specify comma delimited list of range servers")
 	optTTE          = golf.DurationP('e', "tte", 12*time.Hour, "max duration prior to cache eviction")
 )
 
 func main() {
 	golf.Parse()
 
+	if *optHelp {
+		fmt.Fprintf(os.Stderr, "%s\n", filepath.Base(os.Args[0]))
+		if *optHelp {
+			fmt.Fprintf(os.Stderr, "\trun a reverse proxy against one or more range servers\n\n")
+			fmt.Fprintf(os.Stderr, "For bug reports or feature requests:\n")
+			fmt.Fprintf(os.Stderr, "\t* ask about `range-proxy` in #golang\n")
+			fmt.Fprintf(os.Stderr, "\t* send email to govt@linkedin.com\n\n")
+			golf.Usage()
+		}
+		os.Exit(0)
+	}
+
 	servers := strings.Split(*optServers, ",")
 	if servers[0] == "" {
-		servers = []string{"range"} // TODO: put one or more actual range server addresses here
+		fmt.Fprintf(os.Stderr, "ERROR: cannot proxy to unspecified servers\n")
+		os.Exit(2)
 	}
 
 	if *optPprof > 0 {
@@ -44,7 +59,7 @@ func main() {
 		Log:     os.Stderr,
 		Port:    *optPort,
 		Servers: servers,
-		Timeout: 1 * time.Minute, // how long to wait for upstream to respond
+		Timeout: 1 * time.Minute, // how long to wait for downstream to respond
 		TTE:     *optTTE,
 	}))
 }
